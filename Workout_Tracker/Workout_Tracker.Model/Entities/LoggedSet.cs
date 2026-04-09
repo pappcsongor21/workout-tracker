d1f0c266fbd5cebfc98cfa88b108cff8ee16a8e8
Csongor Papp <papp.csongor221@gmail.com>
2026-04-09 17:02:04 +0200

    add LoggedSet entity

---
+++ Workout_Tracker/Workout_Tracker.Model/Entities/LoggedSet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workout_Tracker.Model.Entities
+{
+    public class LoggedSet
+    {
+        public int Id { get; set; }
+        public int ExerciseId { get; set; }
+        public Exercise Exercise { get; set; }
+
+        public int OrderNum {  get; set; }
+        public int TargetReps { get; set; }
+        public int TargetIntensity { get; set; }
+        public int ActualReps { get; set; }
+        public int ActualIntensity { get; set; }
+        public int RPE { get; set; }
+    }
+}
