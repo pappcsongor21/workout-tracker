390aa98a11712a7a3244d13e805b118eee428ff4
Csongor Papp <papp.csongor221@gmail.com>
2026-04-09 17:21:18 +0200

    migrate new entities

---
+++ Workout_Tracker/Workout_Tracker.Persistence/Migrations/20260409151649_InitializeWorkoutSchema.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Workout_Tracker.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitializeWorkoutSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Workouts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Notes = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workouts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkoutTemplates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    ColorNum = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkoutTemplates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkoutSets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ExerciseId = table.Column<int>(type: "INTEGER", nullable: false),
+                    OrderNum = table.Column<int>(type: "INTEGER", nullable: false),
+                    TargetReps = table.Column<int>(type: "INTEGER", nullable: false),
+                    TargetIntensity = table.Column<int>(type: "INTEGER", nullable: false),
+                    ActualReps = table.Column<int>(type: "INTEGER", nullable: false),
+                    ActualIntensity = table.Column<int>(type: "INTEGER", nullable: false),
+                    RPE = table.Column<int>(type: "INTEGER", nullable: false),
+                    WorkoutId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkoutSets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkoutSets_Exercises_ExerciseId",
+                        column: x => x.ExerciseId,
+                        principalTable: "Exercises",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WorkoutSets_Workouts_WorkoutId",
+                        column: x => x.WorkoutId,
+                        principalTable: "Workouts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TemplateExercises",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ExerciseId = table.Column<int>(type: "INTEGER", nullable: false),
+                    WorkoutTemplateId = table.Column<int>(type: "INTEGER", nullable: false),
+                    OrderNum = table.Column<int>(type: "INTEGER", nullable: false),
+                    TargetSets = table.Column<int>(type: "INTEGER", nullable: false),
+                    TargetRepsMin = table.Column<int>(type: "INTEGER", nullable: false),
+                    TargetRepsMax = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TemplateExercises", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TemplateExercises_Exercises_ExerciseId",
+                        column: x => x.ExerciseId,
+                        principalTable: "Exercises",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TemplateExercises_WorkoutTemplates_WorkoutTemplateId",
+                        column: x => x.WorkoutTemplateId,
+                        principalTable: "WorkoutTemplates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TemplateExercises_ExerciseId",
+                table: "TemplateExercises",
+                column: "ExerciseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TemplateExercises_WorkoutTemplateId",
+                table: "TemplateExercises",
+                column: "WorkoutTemplateId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkoutSets_ExerciseId",
+                table: "WorkoutSets",
+                column: "ExerciseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkoutSets_WorkoutId",
+                table: "WorkoutSets",
+                column: "WorkoutId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TemplateExercises");
+
+            migrationBuilder.DropTable(
+                name: "WorkoutSets");
+
+            migrationBuilder.DropTable(
+                name: "WorkoutTemplates");
+
+            migrationBuilder.DropTable(
+                name: "Workouts");
+        }
+    }
+}
