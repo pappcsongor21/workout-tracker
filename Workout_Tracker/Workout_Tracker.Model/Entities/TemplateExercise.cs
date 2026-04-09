893416b0f1b0c6ff4f7aa0077cf5297041406fe6
Csongor Papp <papp.csongor221@gmail.com>
2026-04-09 16:44:27 +0200

    add Template Exercise entity

---
+++ Workout_Tracker/Workout_Tracker.Model/Entities/TemplateExercise.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workout_Tracker.Model.Entities
+{
+    public class TemplateExercise
+    {
+        public int Id { get; set; }
+
+        public int ExerciseId { get; set;  }
+        public Exercise Exercise { get; set; }
+
+        public int WorkoutTemplateId { get; set; }
+        public WorkoutTemplate WorkoutTemplate { get; set; }
+
+        public int OrderNum { get; set; }
+        public int TargetSets { get; set; }
+        public int TargetRepsMin { get; set; }
+        public int TargetRepsMax { get; set; }
+    }
+}
--- Workout_Tracker/Workout_Tracker.Persistence/AppDbContext.cs
+++ Workout_Tracker/Workout_Tracker.Persistence/AppDbContext.cs
@@ -11,6 +11,7 @@ namespace Workout_Tracker.Persistence
     public class AppDbContext : DbContext
     {
         public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<TemplateExercise> TemplateExercises { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
