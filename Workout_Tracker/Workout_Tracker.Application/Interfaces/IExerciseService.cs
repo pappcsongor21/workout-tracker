f244e381e6782f9a0695562ebe99c3453e956e34
Csongor Papp <papp.csongor221@gmail.com>
2026-04-10 09:52:22 +0200

    feat(application): add missing crud methods to IExerciseService using DTO types

--- Workout_Tracker/Workout_Tracker.Application/Interfaces/IExerciseService.cs
+++ Workout_Tracker/Workout_Tracker.Application/Interfaces/IExerciseService.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Workout_Tracker.Application.DTOs.Exercise;
 using Workout_Tracker.Model.Entities;
 
 namespace Workout_Tracker.Application.Interfaces
 {
     public interface IExerciseService
     {
-        Task<List<Exercise>> GetAllExercises();
-        Task CreateExercise(Exercise exercise);
+        Task<List<ExerciseResponse>> GetAllExercises();
+        Task<ExerciseResponse> GetExerciseById(int id);
+        Task<ExerciseResponse> CreateExercise(CreateExerciseRequest exerciseDto);
+        Task UpdateExercise(UpdateExerciseRequest exerciseDto);
+        Task DeleteExercise(int id);
     }
 }
