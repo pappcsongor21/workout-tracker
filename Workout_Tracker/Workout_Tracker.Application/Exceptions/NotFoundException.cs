3785416fc41131a2986d2f5331a7225550b0034b
Csongor Papp <papp.csongor221@gmail.com>
2026-04-10 14:56:00 +0200

    feat(application): add NotFoundException class
    The final goal is global exception handling

---
+++ Workout_Tracker/Workout_Tracker.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿namespace Workout_Tracker.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+
+        public NotFoundException(string entityName, object key)
+            : base($"Item ({entityName}) with the given id ({key}) is not found")
+        {
+        }
+    }
+}
