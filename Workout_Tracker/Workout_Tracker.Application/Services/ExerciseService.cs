2a723359d330e9698c471812917d2a73ddb67edb
Csongor Papp <papp.csongor221@gmail.com>
2026-04-10 15:27:55 +0200

    feat(application): add CRUD method implementations to ExerciseService
    - add GetAllExercisesAsync
    - add GetExerciseByIdAsync
    - add CreateExerciseAsync
    - add UpdateExerciseAsync
    - add DeleteExerciseAsync

--- Workout_Tracker/Workout_Tracker.Application/Services/ExerciseService.cs
+++ Workout_Tracker/Workout_Tracker.Application/Services/ExerciseService.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Workout_Tracker.Application.DTOs.Exercise;
+using Workout_Tracker.Application.Exceptions;
 using Workout_Tracker.Application.Interfaces;
 using Workout_Tracker.Model.Entities;
 using Workout_Tracker.Persistence;
@@ -19,15 +16,77 @@ namespace Workout_Tracker.Application.Services
             _context = context;
         }
 
-        public async Task CreateExercise(Exercise exercise)
+        public async Task<IEnumerable<ExerciseResponse>> GetAllExercisesAsync()
         {
-            _context.Exercises.Add(exercise);
+            var exercises = await _context.Exercises.ToListAsync();
+            var exerciseDtos = exercises.Select(e => new ExerciseResponse
+            {
+                Id = e.Id,
+                Name = e.Name,
+                MuscleGroup = e.MuscleGroup,
+                Description = e.Description
+            }).ToList();
+
+            return exerciseDtos;
+        }
+
+        public async Task<ExerciseResponse> GetExerciseByIdAsync(int id)
+        {
+            var exercise = await _context.Exercises.FirstOrDefaultAsync(e => e.Id == id);
+
+            if (exercise == null) throw new NotFoundException(nameof(Exercise), id);
+
+
+            return new ExerciseResponse
+            {
+                Id = exercise.Id,
+                Name = exercise.Name,
+                MuscleGroup = exercise.MuscleGroup,
+                Description = exercise.Description
+            };
+        }
+
+        public async Task<ExerciseResponse> CreateExerciseAsync(CreateExerciseRequest exerciseDto)
+        {
+            var newExercise = new Exercise
+            {
+                Name = exerciseDto.Name,
+                MuscleGroup = exerciseDto.MuscleGroup,
+                Description = exerciseDto.Description
+            };
+            await _context.AddAsync(newExercise);
+            await _context.SaveChangesAsync();
+
+            return new ExerciseResponse
+            {
+                Id = newExercise.Id,
+                Name = newExercise.Name,
+                MuscleGroup = newExercise.MuscleGroup,
+                Description = newExercise.Description
+            };
+        }
+
+        public async Task UpdateExerciseAsync(UpdateExerciseRequest exerciseDto)
+        {
+            var exercise = await _context.Exercises.FirstOrDefaultAsync(e => e.Id == exerciseDto.Id);
+
+            if (exercise == null) throw new NotFoundException(nameof(Exercise), exerciseDto.Id);
+
+            exercise.Name = exerciseDto.Name;
+            exercise.MuscleGroup = exerciseDto.MuscleGroup;
+            exercise.Description = exerciseDto.Description;
+
             await _context.SaveChangesAsync();
         }
 
-        public async Task<List<Exercise>> GetAllExercises()
+        public async Task DeleteExerciseAsync(int id)
         {
-            return await _context.Exercises.ToListAsync();
+            var exercise = await _context.Exercises.FirstOrDefaultAsync(e => e.Id == id);
+
+            if (exercise == null) throw new NotFoundException(nameof(Exercise), id);
+
+            _context.Exercises.Remove(exercise);
+            await _context.SaveChangesAsync();
         }
     }
 }
