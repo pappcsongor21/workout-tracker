4837eef303dd54dd1aa8300ecb75a2ae76edec4b
Csongor Papp <papp.csongor221@gmail.com>
2026-04-10 09:53:52 +0200

    feat(api): add missing crud methods to ExercisesController

--- Workout_Tracker/Workout_Tracker.Api/Controllers/ExercisesController.cs
+++ Workout_Tracker/Workout_Tracker.Api/Controllers/ExercisesController.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SQLitePCL;
+using Workout_Tracker.Application.DTOs.Exercise;
 using Workout_Tracker.Application.Interfaces;
-using Workout_Tracker.Model.Entities;
 
 namespace Workout_Tracker.Api.Controllers
 {
@@ -18,18 +18,43 @@ namespace Workout_Tracker.Api.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetExercises()
+        public async Task<ActionResult<IEnumerable<ExerciseResponse>>> GetExercisesAsync()
         {
             var exercises = await _service.GetAllExercises();
             return Ok(exercises);
         }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateExercise(Exercise exercise)
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetExerciseByIdAsync(int id)
         {
-            await _service.CreateExercise(exercise);
-
+            var exercise = await _service.GetExerciseById(id);
             return Ok(exercise);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ExerciseResponse>> CreateExerciseAsync(CreateExerciseRequest exercise)
+        {
+            var createdExercise = await _service.CreateExercise(exercise);
+
+            return CreatedAtAction(
+                nameof(GetExerciseByIdAsync), 
+                new {id = createdExercise.Id}, 
+                createdExercise);
+            //here nameof(method) results in a location(url with an id) at the end
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateExerciseAsync(int id, UpdateExerciseRequest exerciseDto)
+        {
+            await _service.UpdateExercise(exerciseDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExerciseAsync(int id)
+        {
+            await _service.DeleteExercise(id);
+            return NoContent();
         }
     }
 }
