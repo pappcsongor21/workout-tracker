3a1e920015e8a54f931ffd05e861f7d6f0e57d9a
Csongor Papp <papp.csongor221@gmail.com>
2026-04-10 11:30:42 +0200

    reafactor(application): change ExerciseResponse from a positional record to a property-based record

--- Workout_Tracker/Workout_Tracker.Application/DTOs/Exercise/ExerciseResponse.cs
+++ Workout_Tracker/Workout_Tracker.Application/DTOs/Exercise/ExerciseResponse.cs
@@ -1,6 +1,10 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace Workout_Tracker.Application.DTOs.Exercise
+﻿namespace Workout_Tracker.Application.DTOs.Exercise
 {
-    public record ExerciseResponse(int Id, string Name, string MuscleGroup, string Description);
+    public record ExerciseResponse
+    {
+        public int Id { get; init; }
+        public string Name { get; init; } = string.Empty;
+        public string MuscleGroup { get; init; } = string.Empty;
+        public string Description { get; init; } = string.Empty;
+    }
 }
