487da4cb160811465cb1a9476e9b03871d0f2c70
Csongor Papp <papp.csongor221@gmail.com>
2026-04-14 10:16:47 +0200

    chore(db): add migration for workout template color to hex conversion

---
+++ Workout_Tracker/Workout_Tracker.Persistence/Migrations/20260414080837_UpdateWorokutTemplateColorToHex.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Workout_Tracker.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateWorokutTemplateColorToHex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ColorNum",
+                table: "WorkoutTemplates");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ColorHex",
+                table: "WorkoutTemplates",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ColorHex",
+                table: "WorkoutTemplates");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ColorNum",
+                table: "WorkoutTemplates",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
