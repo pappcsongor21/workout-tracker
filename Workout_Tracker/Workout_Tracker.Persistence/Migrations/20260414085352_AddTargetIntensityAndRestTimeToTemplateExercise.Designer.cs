9ddc835cbb7511e60fc0700f2a5fe82f5d7ade74
Csongor Papp <papp.csongor221@gmail.com>
2026-04-14 10:56:41 +0200

    chore(db): add migration for new properties (TargetIntensity, RestTime) in TemplateExercise

--- Workout_Tracker/Workout_Tracker.Persistence/Migrations/20260414085352_AddTargetIntensityAndRestTimeToTemplateExercise.Designer.cs
+++ Workout_Tracker/Workout_Tracker.Persistence/Migrations/20260414085352_AddTargetIntensityAndRestTimeToTemplateExercise.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Workout_Tracker.Persistence;
 
@@ -10,9 +11,11 @@ using Workout_Tracker.Persistence;
 namespace Workout_Tracker.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260414085352_AddTargetIntensityAndRestTimeToTemplateExercise")]
+    partial class AddTargetIntensityAndRestTimeToTemplateExercise
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.25");
---
+++ Workout_Tracker/Workout_Tracker.Persistence/Migrations/20260414085352_AddTargetIntensityAndRestTimeToTemplateExercise.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Workout_Tracker.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTargetIntensityAndRestTimeToTemplateExercise : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RestTime",
+                table: "TemplateExercises",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TargetIntensity",
+                table: "TemplateExercises",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RestTime",
+                table: "TemplateExercises");
+
+            migrationBuilder.DropColumn(
+                name: "TargetIntensity",
+                table: "TemplateExercises");
+        }
+    }
+}
