ec99c44ef0a8302dce16d4d883f32d84aa35b9df
Csongor Papp <papp.csongor221@gmail.com>
2026-04-14 11:02:31 +0200

    feat(application): add WorkoutTemplateResponse DTO

---
+++ Workout_Tracker/Workout_Tracker.Application/DTOs/WorkoutTemplate/WorkoutTemplateResponse.cs
@@ -0,0 +1,11 @@
+﻿using Workout_Tracker.Application.DTOs.TemplateExercise;
+
+namespace Workout_Tracker.Application.DTOs.WorkoutTemplate;
+
+public record WorkoutTemplateResponse
+{
+    public int Id { get; set; }
+    public string Name { get; init; } = string.Empty;
+    public string ColorHex {  get; init; } = "#808080";
+    public List<TemplateExerciseResponse> Exercises { get; init; } = new List<TemplateExerciseResponse>(); 
+}
