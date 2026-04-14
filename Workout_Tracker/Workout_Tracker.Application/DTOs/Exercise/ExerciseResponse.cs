64a9461f77201e62e98494a31b69f3506f6cd60b
Csongor Papp <papp.csongor221@gmail.com>
2026-04-14 10:33:59 +0200

    feat(application): remove status messages from ExerciseResponse

--- Workout_Tracker/Workout_Tracker.Application/DTOs/Exercise/ExerciseResponse.cs
+++ Workout_Tracker/Workout_Tracker.Application/DTOs/Exercise/ExerciseResponse.cs
@@ -6,6 +6,4 @@ public record ExerciseResponse
     public string Name { get; init; } = string.Empty;
     public string MuscleGroup { get; init; } = string.Empty;
     public string Description { get; init; } = string.Empty;
-    public bool Success { get; init; } = true;
-    public string Message { get; init; } = "Successful query";
 }
