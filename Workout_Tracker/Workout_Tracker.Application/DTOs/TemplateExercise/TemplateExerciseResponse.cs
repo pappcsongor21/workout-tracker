66fa4da628bfc36fe018d432f4f49b085c605bae
Csongor Papp <papp.csongor221@gmail.com>
2026-04-14 10:39:41 +0200

    feat(application): add TemplateExerciseResponse DTO

---
+++ Workout_Tracker/Workout_Tracker.Application/DTOs/TemplateExercise/TemplateExerciseResponse.cs
@@ -0,0 +1,16 @@
+﻿using Workout_Tracker.Application.DTOs.Exercise;
+
+namespace Workout_Tracker.Application.DTOs.TemplateExercise;
+
+public record TemplateExerciseResponse
+{
+    public int Id { get; init; }
+
+    public ExerciseResponse Exercise { get; init; } = null!;
+
+    public int OrderNum { get; set; }
+    public int TargetSets { get; set; }
+    public int TargetRepsMin { get; set; }
+    public int TargetRepsMax { get; set; }
+    public string TargetIntensity { get; set; } = string.Empty;
+}
