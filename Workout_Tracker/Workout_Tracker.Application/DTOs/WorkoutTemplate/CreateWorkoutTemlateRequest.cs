ad09d12b6b0f6944c574774a8ec43a18b4208090
Csongor Papp <papp.csongor221@gmail.com>
2026-04-16 11:59:56 +0200

    feat(application): add DTOs for WorkoutTemplate
    - add DTO for create
    - add DTO for update

---
+++ Workout_Tracker/Workout_Tracker.Application/DTOs/WorkoutTemplate/CreateWorkoutTemlateRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Workout_Tracker.Application.DTOs.WorkoutTemplate;
+
+public record CreateWorkoutTemplateRequest
+{
+    public string Name { get; init; } = string.Empty;
+    public string ColorHex { get; init; } = "#808080";
+    public List<CreateWorkoutTemplateRequest> Exercises { get; init; } = new();
+}
---
+++ Workout_Tracker/Workout_Tracker.Application/DTOs/WorkoutTemplate/UpdateWorkoutTemplateRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Workout_Tracker.Application.DTOs.WorkoutTemplate;
+
+public class UpdateWorkoutTemplateRequest
+{
+    public string Name { get; init; } = string.Empty;
+    public string ColorHex { get; init; } = "#808080";
+    public List<UpdateWorkoutTemplateRequest> Exercises { get; init; } = new();
+}
