eb24ef61c66b0815d70256a772748e0ab9fd0396
Csongor Papp <papp.csongor221@gmail.com>
2026-04-16 12:00:52 +0200

    feat(app): add IWorkoutTemplateService
    It only has CRUD methods yet.

---
+++ Workout_Tracker/Workout_Tracker.Application/Interfaces/IWorkoutTemplateService.cs
@@ -0,0 +1,12 @@
+﻿using Workout_Tracker.Application.DTOs.WorkoutTemplate;
+
+namespace Workout_Tracker.Application.Interfaces;
+
+public interface IWorkoutTemplateService
+{
+    Task<IEnumerable<WorkoutTemplateResponse>> GetWorkoutTemplatesAsync();
+    Task<WorkoutTemplateResponse> GetWorkoutTemplateByIdAsync(int id);
+    Task<WorkoutTemplateResponse> CreateWorkoutTemplateAsync(CreateWorkoutTemplateRequest workoutTemplateDto);
+    Task UpdateWorkoutTemplateAsync(UpdateWorkoutTemplateRequest workoutTemplateDto);
+    Task DeleteWorkoutTemplateAsync(int id);
+}
