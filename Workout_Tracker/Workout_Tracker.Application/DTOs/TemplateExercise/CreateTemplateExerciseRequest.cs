5bc4d363ab54673f23cb9a258ee51331089ad58b
Csongor Papp <papp.csongor221@gmail.com>
2026-04-16 11:59:12 +0200

    feat(application): add DTO-s for TemplateExercise
    - add DTO for create
    - add DTO for update

---
+++ Workout_Tracker/Workout_Tracker.Application/DTOs/TemplateExercise/CreateTemplateExerciseRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Workout_Tracker.Application.DTOs.TemplateExercise;
+
+public record CreateTemplateExerciseRequest
+{
+    public int ExerciseId { get; init; }
+    public int OrderNum { get; set; }
+    public int TargetSets { get; set; }
+    public int TargetRepsMin { get; set; }
+    public int TargetRepsMax { get; set; }
+    public string TargetIntensity { get; set; } = string.Empty;
+    public int RestTime { get; set; }
+}
