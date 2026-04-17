f8477e8701e21a989123b1182d529e1701d762ef
Csongor Papp <papp.csongor221@gmail.com>
2026-04-17 11:46:19 +0200

    fix(application): correct typo in CreateWorkoutTemplateRequest

--- Workout_Tracker/Workout_Tracker.Application/DTOs/WorkoutTemplate/CreateWorkoutTemlateRequest.cs
+++ Workout_Tracker/Workout_Tracker.Application/DTOs/WorkoutTemplate/CreateWorkoutTemlateRequest.cs
@@ -1,8 +1,10 @@
-﻿namespace Workout_Tracker.Application.DTOs.WorkoutTemplate;
+﻿using Workout_Tracker.Application.DTOs.TemplateExercise;
+
+namespace Workout_Tracker.Application.DTOs.WorkoutTemplate;
 
 public record CreateWorkoutTemplateRequest
 {
     public string Name { get; init; } = string.Empty;
     public string ColorHex { get; init; } = "#808080";
-    public List<CreateWorkoutTemplateRequest> Exercises { get; init; } = new();
+    public List<CreateTemplateExerciseRequest> Exercises { get; init; } = new();
 }
