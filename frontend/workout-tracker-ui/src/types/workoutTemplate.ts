ff7318e78ab06a4224e12a897096bd6a866f1bcc
Csongor Papp <papp.csongor221@gmail.com>
2026-05-19 22:42:12 +0200

    feat(types): add WorkoutTemplate and TemplateExercise interfaces

---
+++ frontend/workout-tracker-ui/src/types/workoutTemplate.ts
@@ -0,0 +1,26 @@
+export interface TemplateExercise{
+    id: number;
+    exercise:{
+        id: number;
+        name: string;
+        muscleGroup: string;
+        description: string;
+    }
+    orderNum: number;
+    targetSets: number;
+    targetRepsMin: number;
+    targetRepsMax: number;
+    targetIntensity: string;
+    restSeconds: number;
+}
+
+export interface WorkoutTemplate{
+    id: number;
+    name: string;
+    colorHex: string;
+    exercises: TemplateExercise[];
+}
+
+export interface WorkoutTemplateCardProps {
+    workoutTemplate: WorkoutTemplate;
+}
