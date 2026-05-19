aa116fb1f3365da785a338e24568c455b8349fca
Csongor Papp <papp.csongor221@gmail.com>
2026-05-19 16:19:51 +0200

    feat(frontend): initial commit

--- Workout_Tracker/Workout_Tracker.Application/Services/WorkoutTemplateService.cs
+++ Workout_Tracker/Workout_Tracker.Application/Services/WorkoutTemplateService.cs
@@ -2,6 +2,7 @@
 using Workout_Tracker.Application.DTOs.Exercise;
 using Workout_Tracker.Application.DTOs.TemplateExercise;
 using Workout_Tracker.Application.DTOs.WorkoutTemplate;
+using Workout_Tracker.Application.Exceptions;
 using Workout_Tracker.Application.Interfaces;
 using Workout_Tracker.Model.Entities;
 using Workout_Tracker.Persistence;
@@ -40,16 +41,50 @@ public class WorkoutTemplateService : IWorkoutTemplateService
                         MuscleGroup = e.Exercise.MuscleGroup,
                         Description = e.Exercise.Description
                     }
-                }).ToList() 
+                }).ToList()
             })
-        .ToListAsync(); 
+        .ToListAsync();
 
         return responseDtos;
     }
 
-    public Task<WorkoutTemplateResponse> GetWorkoutTemplateByIdAsync(int id)
+    public async Task<WorkoutTemplateResponse> GetWorkoutTemplateByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        var workoutTemplate = await _context.WorkoutTemplates
+            .Include(w => w.Exercises)
+            .ThenInclude(e => e.Exercise)
+            .FirstOrDefaultAsync(w => w.Id == id);
+
+        if (workoutTemplate == null)
+        {
+            throw new NotFoundException(nameof(WorkoutTemplate), id);
+        }
+
+        var responseDto = new WorkoutTemplateResponse
+        {
+            Id = workoutTemplate.Id,
+            ColorHex = workoutTemplate.ColorHex,
+            Name = workoutTemplate.Name,
+            Exercises = workoutTemplate.Exercises.Select(e => new TemplateExerciseResponse
+            {
+                Id = e.Id,
+                OrderNum = e.OrderNum,
+                TargetSets = e.TargetSets,
+                TargetRepsMin = e.TargetRepsMin,
+                TargetRepsMax = e.TargetRepsMax,
+                TargetIntensity = e.TargetIntensity,
+
+                Exercise = new ExerciseResponse
+                {
+                    Id = e.Exercise.Id,
+                    Name = e.Exercise.Name,
+                    MuscleGroup = e.Exercise.MuscleGroup,
+                    Description = e.Exercise.Description
+                }
+            }).ToList()
+        };
+
+        return responseDto;
     }
 
     public async Task<WorkoutTemplateCreatedResponse> CreateWorkoutTemplateAsync(CreateWorkoutTemplateRequest request)
@@ -91,7 +126,7 @@ public class WorkoutTemplateService : IWorkoutTemplateService
             }).ToList()
         };
 
-        return responseDto;    
+        return responseDto;
     }
 
     public Task DeleteWorkoutTemplateAsync(int id)
