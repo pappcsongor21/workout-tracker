c8039af1fe4484b4a31eb62c6c1f88d684604050
Csongor Papp <papp.csongor221@gmail.com>
2026-05-19 21:49:22 +0200

    chore(db): update templateExercise.restSeconds name in db

---
+++ Workout_Tracker/Workout_Tracker.Persistence/Migrations/20260519190553_renameToRestSeconds.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Workout_Tracker.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class renameToRestSeconds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RestTime",
+                table: "TemplateExercises",
+                newName: "RestSeconds");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RestSeconds",
+                table: "TemplateExercises",
+                newName: "RestTime");
+        }
+    }
+}
