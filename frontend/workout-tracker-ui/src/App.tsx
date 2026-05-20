a3bd25d8f635280a02bbcd77c75d3c00222b9c6e
Csongor Papp <papp.csongor221@gmail.com>
2026-05-20 12:54:01 +0200

    feat(ui): introduce routing

--- frontend/workout-tracker-ui/src/App.tsx
+++ frontend/workout-tracker-ui/src/App.tsx
@@ -1,12 +1,18 @@
-import WorkoutTemplateCards from "./components/WorkoutTemplateCards";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import MainPage from "./pages/MainPage";
 
 const App = () => {
 
-  return(
-    <>
-    <WorkoutTemplateCards/>
-    </>
-  )
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<MainPage/>}/>
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App
