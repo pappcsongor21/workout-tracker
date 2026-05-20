dff6ae247cad790c4af09e60324dbff3843c1a87
Csongor Papp <papp.csongor221@gmail.com>
2026-05-20 13:33:13 +0200

    feat(ui): make navbar a bit more responsive

--- frontend/workout-tracker-ui/src/components/NavBar.tsx
+++ frontend/workout-tracker-ui/src/components/NavBar.tsx
@@ -2,9 +2,13 @@ import { Link } from "react-router-dom"
 
 const NavBar = () => {
   return (
+    <nav className="bg-emerald-700 text-white px-6 py-4 flex justify-between items-center shadow-md border-b-2 border-emerald-800
+    lg:h-24 lg:pr-12 lg:pl-6">
+
     <nav className="bg-emerald-700 text-white px-6 py-4 flex justify-between items-center shadow-md border-b-2 border-emerald-800">
         <div className="flex items-center gap-2 mx-auto">
-          <Link to="/" className="text-2xl font-black tracking-wider uppercase text-white hover:opacity-90 transition-opacity">
+          <Link to="/" className="text-2xl font-black tracking-wider uppercase text-white hover:opacity-90 transition-opacity
+          lg:text-3xl">
             Workout Tracker
           </Link>
         </div>
