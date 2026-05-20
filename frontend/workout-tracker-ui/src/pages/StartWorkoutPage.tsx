58848ad9e5ff6b284c08f35a909aae4c6efd7348
Csongor Papp <papp.csongor221@gmail.com>
2026-05-20 13:15:08 +0200

    feat(ui): add start workout page

---
+++ frontend/workout-tracker-ui/src/pages/StartWorkoutPage.tsx
@@ -0,0 +1,9 @@
+import WorkoutTemplateCards from "../components/WorkoutTemplateCards"
+
+const StartWorkoutPage = () => {
+  return (
+    <WorkoutTemplateCards />
+  )
+}
+
+export default StartWorkoutPage
