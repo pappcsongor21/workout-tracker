541311cd623a77b7e3e627a5881d1f4b3e9d7276
Csongor Papp <papp.csongor221@gmail.com>
2026-05-20 19:32:35 +0200

    feat(ui): add active navigation link highlighting with NavLinks

--- frontend/workout-tracker-ui/src/components/NavBar.tsx
+++ frontend/workout-tracker-ui/src/components/NavBar.tsx
@@ -1,7 +1,13 @@
 import { FaTable } from "react-icons/fa6"
-import { Link } from "react-router-dom"
+import {  NavLink } from "react-router-dom"
 
 const NavBar = () => {
+
+  const linkClass = ({ isActive }:{isActive:boolean}) :string => 
+    isActive
+      ? 'px-4 py-2 rounded-lg text-lg font-bold bg-lime-300 text-black'
+      : 'px-4 py-2 rounded-lg text-lg font-bold transition duration-100 hover:bg-lime-300 hover:text-black'
+
   return (
     <nav className="bg-emerald-700 text-white px-6 py-4 flex justify-between items-center shadow-md border-b-2 border-emerald-800
     lg:h-24 lg:pr-12 lg:pl-6">
@@ -10,19 +16,19 @@ const NavBar = () => {
         lg:text-3xl"/>
 
         <div className="flex items-center gap-2 mx-auto">
-          <Link to="/" className="text-2xl font-black tracking-wider uppercase text-white hover:opacity-90 transition-opacity
+          <NavLink to="/" className="text-2xl font-black tracking-wider uppercase text-white hover:opacity-90 transition-opacity
           lg:text-3xl">
             Workout Tracker
-          </Link>
+          </NavLink>
         </div>
 
         <div className="flex gap-4">
-          <Link
+          <NavLink
             to="/workout/start"
-            className={`px-4 py-2 rounded-lg text-lg font-bold transition duration-100 hover:bg-lime-300 hover:text-black`}
+            className={linkClass}
           >
             Start workout
-          </Link>
+          </NavLink>
         </div>
       </nav>
   )
