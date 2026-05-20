a65aa5fb39c7d57784f6892d9dcb551adffe9a41
Csongor Papp <papp.csongor221@gmail.com>
2026-05-20 12:52:37 +0200

    feat(ui): create main page

---
+++ frontend/workout-tracker-ui/src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import WorkoutTemplateCards from "../components/WorkoutTemplateCards"
+
+const MainPage = () => {
+  return (
+    <>
+      <WorkoutTemplateCards />
+    </>
+  )
+}
+
+export default MainPage
