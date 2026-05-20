24958982a60013c6bbefe6b4d3143bed1411a76e
Csongor Papp <papp.csongor221@gmail.com>
2026-05-20 13:33:36 +0200

    feat(ui): add logo to navbar

--- frontend/workout-tracker-ui/src/components/NavBar.tsx
+++ frontend/workout-tracker-ui/src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { FaTable } from "react-icons/fa6"
 import { Link } from "react-router-dom"
 
 const NavBar = () => {
@@ -5,7 +6,9 @@ const NavBar = () => {
     <nav className="bg-emerald-700 text-white px-6 py-4 flex justify-between items-center shadow-md border-b-2 border-emerald-800
     lg:h-24 lg:pr-12 lg:pl-6">
 
-    <nav className="bg-emerald-700 text-white px-6 py-4 flex justify-between items-center shadow-md border-b-2 border-emerald-800">
+        <FaTable className="text-emerald-300 text-2xl font-light
+        lg:text-3xl"/>
+
         <div className="flex items-center gap-2 mx-auto">
           <Link to="/" className="text-2xl font-black tracking-wider uppercase text-white hover:opacity-90 transition-opacity
           lg:text-3xl">
