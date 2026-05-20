51ff7ac8af8bf3e32b248742a76aa19cba1a321d
Csongor Papp <papp.csongor221@gmail.com>
2026-05-20 11:39:45 +0200

    refactor(app): move workoutTempCard management to its own component

--- frontend/workout-tracker-ui/src/App.tsx
+++ frontend/workout-tracker-ui/src/App.tsx
@@ -1,45 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
-import WorkoutTemplateCard from "./components/WorkoutTemplateCard";
-import type {WorkoutTemplate} from "./types/workoutTemplate";
-
-const fetchWorkoutTemplates = async (): Promise<WorkoutTemplate[]> =>{
-    const res = await fetch('/api/WorkoutTemplates');
-    if(!res.ok){
-      throw new Error('error fetching data')
-    }
-  return res.json();
-}
+import WorkoutTemplateCards from "./components/WorkoutTemplateCards";
 
 const App = () => {
 
-  const {data: workoutTemplates, isLoading, isError} = useQuery({
-    queryKey: ['workoutTemplates'],
-    queryFn: fetchWorkoutTemplates,
-  })
-
-  if(isLoading) {
-    return (
-      <div className="flex justify-center items-center h-screen text-2xl font-semibold">
-        Loading workout templates...
-      </div>
-    )
-  }
-
-  if(isError) {
-     <div className="flex justify-center items-center h-screen text-2xl text-red-500 font-bold">
-      Cannot fetch workout template data, check the backend
-     </div>
-  }
-
-  return (
-    <div className="p-3 flex flex-wrap gap-6 justify-center">
-      {workoutTemplates?.map((workoutTemplate) => (
-        <WorkoutTemplateCard 
-        key={workoutTemplate.id}
-        workoutTemplate={workoutTemplate} />
-      ))}
-    </div>
-
+  return(
+    <>
+    <WorkoutTemplateCards/>
+    </>
   )
 }
 
---
+++ frontend/workout-tracker-ui/src/components/WorkoutTemplateCards.tsx
@@ -0,0 +1,45 @@
+import { useQuery } from "@tanstack/react-query";
+import type {WorkoutTemplate} from "../types/workoutTemplate";
+import WorkoutTemplateCard from "./WorkoutTemplateCard";
+
+const fetchWorkoutTemplates = async (): Promise<WorkoutTemplate[]> =>{
+    const res = await fetch('/api/WorkoutTemplates');
+    if(!res.ok){
+      throw new Error('error fetching data')
+    }
+  return res.json();
+}
+
+const WorkoutTemplateCards = () => {
+
+        const {data: workoutTemplates, isLoading, isError} = useQuery({
+        queryKey: ['workoutTemplates'],
+        queryFn: fetchWorkoutTemplates,
+        })
+
+    if(isLoading) {
+        return (
+        <div className="flex justify-center items-center h-screen text-2xl font-semibold">
+            Loading workout templates...
+        </div>
+        )
+    }
+
+    if(isError) {
+        <div className="flex justify-center items-center h-screen text-2xl text-red-500 font-bold">
+        Cannot fetch workout template data, check the backend
+        </div>
+    }
+
+    return (
+        <div className="p-3 flex flex-wrap gap-6 justify-center">
+        {workoutTemplates?.map((workoutTemplate) => (
+            <WorkoutTemplateCard 
+            key={workoutTemplate.id}
+            workoutTemplate={workoutTemplate} />
+        ))}
+        </div>
+    )
+}
+
+export default WorkoutTemplateCards
