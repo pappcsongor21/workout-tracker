75e87d4b01bd2c4f796cde32d09b8d02097febe1
Csongor Papp <papp.csongor221@gmail.com>
2026-05-21 14:26:24 +0200

    feat(schema): add createWorkoutTemplateSchema for workout templates

---
+++ frontend/workout-tracker-ui/src/schemas/workout.ts
@@ -0,0 +1,26 @@
+import * as z from 'zod';
+
+export const createWorkoutTemplateSchema = z.object({
+    name: z.string().trim()
+    .min(1, 'Template name is required.')
+    .max(25, 'Name is too long.'),
+    colorHex: z.string().regex(/^#[0-9A-Fa-f]{6}$/, 'invalid color code.'),
+
+    exercises: z.array(
+        z.object({
+            exerciseId: z.coerce.number().min(1, 'Choose an exercise!'),
+
+            targetSets: z.coerce.number().min(1, 'Minimum 1 set.'),
+            targetRepsMin: z.coerce.number().min(1, 'Minimum 1 repetition.'),
+            targetRepsMax: z.coerce.number().min(1, 'Minimum 1 repetition.'),
+
+            targetIntensity: z.string().trim().optional(),
+            restSeconds: z.coerce.number().min(0, 'Rest time cannot be negative.')
+        }).refine((exercise) => exercise.targetRepsMax >= exercise.targetRepsMin, {
+          message: 'Maximum reps cannot be less than minimum reps!',
+          path: ['targetRepsMax'],
+        })
+    )
+});
+
+export type CreateWorkoutTemplateFormData = z.infer<typeof createWorkoutTemplateSchema>;
