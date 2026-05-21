4dcc3795a73835fd74234c74e22c4b73e40e94ed
Csongor Papp <papp.csongor221@gmail.com>
2026-05-21 11:01:51 +0200

    fix(dto): change Exercises to list of UpdateTemplateExerciseRequest dto

--- Workout_Tracker/Workout_Tracker.Application/DTOs/WorkoutTemplate/UpdateWorkoutTemplateRequest.cs
+++ Workout_Tracker/Workout_Tracker.Application/DTOs/WorkoutTemplate/UpdateWorkoutTemplateRequest.cs
@@ -1,8 +1,10 @@
-﻿namespace Workout_Tracker.Application.DTOs.WorkoutTemplate;
+﻿using Workout_Tracker.Application.DTOs.TemplateExercise;
+
+namespace Workout_Tracker.Application.DTOs.WorkoutTemplate;
 
 public class UpdateWorkoutTemplateRequest
 {
     public string Name { get; init; } = string.Empty;
     public string ColorHex { get; init; } = "#808080";
-    public List<UpdateWorkoutTemplateRequest> Exercises { get; init; } = new();
+    public List<UpdateTemplateExerciseRequest> Exercises { get; init; } = new();
 }
